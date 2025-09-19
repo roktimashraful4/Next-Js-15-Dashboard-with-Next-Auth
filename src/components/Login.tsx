138b4217671fa128fe6a6dee59ffb90f90ed1ea4
roktimashraful <roktimshraful4@gmail.com>
2025-09-19 21:23:22 +0600

    add loading in login button

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { login } from "@/utils/actions";
 import Image from "next/image";
-import { JSX } from "react";
+import { JSX, useState } from "react";
 
 interface Props {
   type: string;
@@ -9,11 +9,22 @@ interface Props {
 }
 
 export default function Login({ type }: Props): JSX.Element {
+    const [loading, setLoading] =  useState(false);
+
+    const handleLogin = async (type: string) => {
+        setLoading(true);
+        try {
+            await login(type);
+        } catch (error) {
+            console.error("Login failed:", error);
+            setLoading(false);
+        }
+    }
   if (type === "google") {
       return (
-        <button onClick={() => login(type)} className="border border-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline cursor-pointer">
+        <button onClick={() => handleLogin(type)} disabled={loading } className="border border-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline cursor-pointer">
             <Image src="/google.svg" alt="Google Logo" width={20} height={20} className="inline-block mr-2" />
-            Sign in with Google
+            {loading ? "Loading..." : "Sign in with Google"}
             </button>
       );
   } 
